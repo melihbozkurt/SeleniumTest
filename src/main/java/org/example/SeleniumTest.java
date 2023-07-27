266286f0ec179dec8e2232ab747992e114c23995
Melih Can Bozkurt <melihcanbozkurt07@hotmail.com>
2023-07-27 19:24:47 +0300

    test1

---
+++ src/main/java/org/example/SeleniumTest.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+
+    public static void main(String[] args) {
+
+        // 1- Setting up the web driver manager
+
+        // WebDriverManager.chromedriver().setup();
+
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Jakobiyan\\Desktop\\chromedriver.exe");
+
+        // 2- Create instance of the chrome driver
+
+        WebDriver driver = new ChromeDriver();
+
+        // 3- Test if driver is working as expected
+        driver.get("https://www.google.com");
+
+    }
+}
