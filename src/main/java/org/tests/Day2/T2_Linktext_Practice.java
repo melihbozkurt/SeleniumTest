e71c7ff86ee9375025d4671cb7c3dfa8922afe38
Melih Can Bozkurt <melihcanbozkurt07@hotmail.com>
2023-07-31 02:37:08 +0300

    Day2

---
+++ src/main/java/org/tests/Day2/T2_Linktext_Practice.java
@@ -0,0 +1,16 @@
+package org.tests.Day2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T2_Linktext_Practice {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+
+    }
+}
