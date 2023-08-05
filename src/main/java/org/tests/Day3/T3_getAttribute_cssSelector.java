1b705c10449bc27d1c85f313bee67ce899cc0cba
Melih Can Bozkurt <melihcanbozkurt07@hotmail.com>
2023-08-05 03:34:31 +0300

    t3

---
+++ src/main/java/org/tests/Day3/T3_getAttribute_cssSelector.java
@@ -0,0 +1,47 @@
+package org.tests.Day3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.utilities.WebDriverFactory;
+
+public class T3_getAttribute_cssSelector {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+
+        //2- Go to: https://login1.nextbasecrm.com/
+        driver.get("https://login1.nextbasecrm.com/");
+
+        //3- Verify “Log in” button text is as expected:
+        //Expected: Log In
+        //WebElement signInButton = driver.findElement(By.className("login-btn"));
+
+        //LOCATING THE SAME WEB ELEMENT USING DIFFERENT ATTRIBUTE VALUES.
+        //	                   tagName[attribute='value']
+        //	                   input[class='login-btn']
+        // LOCATED USING CLASS ATTRIBUTE
+        //WebElement signInButton = driver.findElement(By.cssSelector("input[class='login-btn']"));
+        // LOCATED USING TYPE ATTRIBUTE
+        //WebElement signInButton = driver.findElement(By.cssSelector("input[type='submit']"));
+        // LOCATED USING VALUE ATTRIBUTE
+        WebElement signInButton = driver.findElement(By.cssSelector("input[value='Log In']"));
+
+        String expectedButtonText = "Log In";
+
+        //Getting the value of the attribute "value"
+        String actualButtonText = signInButton.getAttribute("value");
+
+        System.out.println("actualButtonText = " + actualButtonText);
+
+        if (actualButtonText.equals(expectedButtonText)){
+            System.out.println("Log In button text verification passed!");
+        }else{
+            System.out.println("Log In button text verification failed!");
+        }
+
+    }
+
+}
